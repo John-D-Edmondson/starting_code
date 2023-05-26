100d7f67a41549a1874daa69708abfa85fdb8f6b
John-D-Edmondson <john.edmondson@students.plymouth.ac.uk>
2023-05-26 09:55:25 +0100

    added routing and routes to 5 paths

--- src/app/App.js
+++ src/app/App.js
@@ -10,23 +10,26 @@ import Profile from "../components/Profile";
 import EditProfileForm from "../components/EditProfileForm";
 import Root from "../components/Root";
 
-import { Route } from 'react-router-dom';
+import { RouteProvider, Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Router } from 'react-router-dom';
 
 import "./App.css";
 
-const router = 
-  /* Wrap this Root Route to create Router here */
-  <Route path="/" element={ <Root/> }>
-    {/* Add Routes here! */}
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={ <Root/> }>  {/* parent route */ }
+    <Route path='about' element={ <About/> }/> {/* child route with path relative to parent ('/') */}
+    <Route path="sign-up" element={<SignUp/>}/>
+    <Route path="articles" element={<Articles/>}/>
+    <Route path="categories" element={<Categories/>}/>
+    <Route path="profile" element={<Profile/>}/>
   </Route>
+));
+
 
 function App() {
   return (
-    <>
-      { /* Replace below and add Router Provider*/}
-      <p>REPLACE ME WITH A ROUTER PROVIDER</p>
-    </>
+      <RouterProvider router={appRouter} />
   );
 }
 
 export default App;
+ 
